883381a5e378afbc35cac4bb028f5eee509e76bc
vipi-git <62180128+vipi-git@users.noreply.github.com>
2023-07-31 11:11:32 +0530

    Update 2

--- sample_encrypt.py
+++ sample_encrypt.py
@@ -33,3 +33,29 @@ os.environ['API_KEY'] = 'your_api_key_here'
 # Access the API key in your code
 api_key = os.environ.get('API_KEY')
 print(api_key)
+
+# read encryption key from environment variables and decrypt 
+
+import os
+from cryptography.fernet import Fernet
+
+# Read the encryption key from the environment variable
+encryption_key_str = os.environ.get('ENCRYPTION_KEY')
+if not encryption_key_str:
+    raise ValueError("Encryption key not found in the environment variable.")
+
+# Convert the encryption key from string to bytes
+encryption_key = bytes.fromhex(encryption_key_str)
+
+# Read the encrypted data from the file
+with open('encrypted_data.bin', 'rb') as file:
+    encrypted_data = file.read()
+
+# Decrypt the data
+fernet = Fernet(encryption_key)
+decrypted_data = fernet.decrypt(encrypted_data).decode()
+
+# Print the decrypted data
+print(decrypted_data)
+
+
